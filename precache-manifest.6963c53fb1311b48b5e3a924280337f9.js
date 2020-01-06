9e5f3aea1eb878e1707cbdcfd6779046e2c5c793
oussamaFst <g.oussamaww2@gmail.com>
2020-01-06 16:28:47 +0100

    Updates

---
+++ precache-manifest.6963c53fb1311b48b5e3a924280337f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efcd5e6d217a56d125e820d65f122192",
+    "url": "/monster-rolodex/index.html/index.html"
+  },
+  {
+    "revision": "bdc79dfaa73109067d27",
+    "url": "/monster-rolodex/index.html/static/css/main.a4518692.chunk.css"
+  },
+  {
+    "revision": "4c663594a9bc41ed152c",
+    "url": "/monster-rolodex/index.html/static/js/2.47229414.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/index.html/static/js/2.47229414.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bdc79dfaa73109067d27",
+    "url": "/monster-rolodex/index.html/static/js/main.2ec84999.chunk.js"
+  },
+  {
+    "revision": "82802dc317fb8c6ed4b2",
+    "url": "/monster-rolodex/index.html/static/js/runtime-main.f4fcbc1a.js"
+  }
+]);
